f6146288ff1392da2673bf5f4369d3ba3a2f94ef
Marcos Ernesto <marcosresidentevil@hotmail.com>
2019-01-16 21:08:16 -0600

    Voy en el video de yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,11 @@
-const { crearArchivo } = require ('./multiplicar/multiplicar.js')
-
-
-//console.log(process.argv)
+const { crearArchivo } = require ('./multiplicar/multiplicar.js')npm install --save-dev nodemon
 
 let argv = process.argv;
 let parametro = argv[2];
 let base = parametro.split('=')[1];
 
-console.log(base);
 
-/*crearArchivo(9)
+crearArchivo(base)
     .then(archivo => console.log(`el archivo ${archivo} fue creado`))
     .catch(e=>console.log(e));
-*/
+
