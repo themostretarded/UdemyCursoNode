3160f0ea17fa19ec07a9b6144856a6d4fdaf0b45
Marcos Ernesto <marcosresidentevil@hotmail.com>
2019-01-30 21:08:28 -0600

    seccion 6 clase 56
    hasta ahi voy

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,4 +1,6 @@
 
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion:{
         alias: 'd',
@@ -7,4 +9,19 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+//console.log(argv.direccion);
+
+let encodedUrl = encodeURI(argv.direccion);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodedUrl }&key=AIzaSyAu2rb0mobiznVJnJd6bVb5Bn2WsuXP2QI`)
+    .then(resp =>{
+        let location = resp.data.results[0];
+        let coors = location.geometry.location;
+        //location.geometry.location.lat,location.geometry.location.lng
+        console.log(`Direccion: ${location.formatted_address}`);
+        console.log(`lat:${coors.lat}`);
+        console.log(`lng:${coors.lng}`);
+        //console.log(JSON.stringify(resp.data,undefined,2));
+        //console.log(resp.status);
+    })
+    .catch(e => console.log('ERROR !!!!',e));
