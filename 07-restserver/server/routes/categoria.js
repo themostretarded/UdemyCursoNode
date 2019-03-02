e8af8b8aa6ca7f2365045acfa449abbb9345bab3
Marcos Ernesto <marcosresidentevil@hotmail.com>
2019-03-01 20:47:14 -0600

    seccion 12 clase 143 terminada

--- 07-restserver/server/routes/categoria.js
+++ 07-restserver/server/routes/categoria.js
@@ -14,6 +14,8 @@
  //  ==================
  app.get('/categoria',verificaToken, (req, res) => {
     Categoria.find({})
+                .sort('descripcion')
+                .populate('usuario','nombre email')
                 .exec((err,categorias)=>{
                     if (err) {
                         return res.status(500).json({
